2294d30eba833bed79d6b3811468048fe72877b7
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-12-01 21:09:04 +0000

    Code cleanup & parametrization

--- lib/imagize.py
+++ lib/imagize.py
@@ -8,11 +8,16 @@ class Imagize(object):
     font_size = 24
     image_height = 150
     image_width = 600
+    color = "#000"
+    bgcolor = "#FFF"
+    left_padding = 6
+    right_padding = 6
+    width = 600
 
     def __init__(self):
         pass
 
-    def generate(self, text, color="#000", bgcolor="#FFF", leftpadding=6, rightpadding=6, width=600):
+    def generate(self, text):
         replacement_character = u'\uFFFD'
         newline_replacement_string = ' ' + replacement_character + ' '
 
@@ -31,36 +36,34 @@ class Imagize(object):
 
         for word in text.split():
             if word == replacement_character: # give a blank line
-                lines.append( line[1:] ) # slice the white space in the begining of the line
+                lines.append( line[1:] ) # slice the white space in the beginning of the line
                 line = u""
                 lines.append( u"" ) # the blank line
-            elif font.getsize( line + ' ' + word )[0] <= (width - rightpadding - leftpadding):
+            elif font.getsize(line + ' ' + word)[0] <= (self.width - self.right_padding - self.left_padding):
                 line += ' ' + word
             else: # start a new line
-                lines.append( line[1:] ) # slice the white space in the begining of the line
+                lines.append( line[1:] ) # slice the white space at the beginning of the line
                 line = u""
-
-                #TODO: handle too long words at this point
                 line += ' ' + word # for now, assume no word alone can exceed the line width
 
         if len(line) != 0:
             lines.append( line[1:] ) # add the last line
 
         line_height = font.getsize(text)[1]
-        img_height = line_height * (len(lines) + 1)
+        img_height = int(line_height * (len(lines) + 1.2))
 
-        img = Image.new("RGBA", (width, img_height), bgcolor)
+        img = Image.new("RGBA", (self.width, img_height), self.bgcolor)
         draw = ImageDraw.Draw(img)
 
-        y = 0
+        y_coord_line_start = 0
         for line in lines:
-            draw.text( (leftpadding, y), line, color, font=font)
-            y += line_height
+            draw.text( (self.left_padding, y_coord_line_start), line, self.color, font=font)
+            y_coord_line_start += line_height
 
         # add linkback at the bottom
-        draw.text( (width - linkbackx, img_height - linkback_height), linkback, color, font=fontlinkback)
+        draw.text( (self.width - linkbackx, img_height - linkback_height), linkback, self.color, font=fontlinkback)
 
-        img = img.resize((int(float(width)/1.02), int(float(img_height)/1.02)), Image.ANTIALIAS)
+        img = img.resize((int(float(self.width)/1.02), int(float(img_height)/1.02)), Image.ANTIALIAS)
 
         filename = "static/" + str(uuid.uuid4()) + ".png"
         img.save(filename, 'PNG', quality=100)
